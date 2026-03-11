532e1f91d5f436f59a03e79666925a58c53a7403
Ajinkya-Pame <pameajinkya@gmail.com>
2026-03-11 21:39:59 +0530

    Comment ecs task definition block

--- infra/modules/ecs_service_database/main.tf
+++ infra/modules/ecs_service_database/main.tf
@@ -3,49 +3,49 @@ resource "aws_cloudwatch_log_group" "database_log_group" {
   retention_in_days = var.RETENTION_DAYS
 }
 
-resource "aws_ecs_task_definition" "database_task" {
-  family                   = "${var.ENVIRONMENT}-${var.SERVICE_NAME}-${var.TASK}"
-  network_mode             = var.NETWORK_MODE
-  requires_compatibilities = [var.REQ_COMPATIBILITY]
-  cpu                      = var.DB_CPU
-  memory                   = var.DB_MEMORY
-  execution_role_arn       = var.EXECUTION_ROLE_ARN
+# resource "aws_ecs_task_definition" "database_task" {
+#   family                   = "${var.ENVIRONMENT}-${var.SERVICE_NAME}-${var.TASK}"
+#   network_mode             = var.NETWORK_MODE
+#   requires_compatibilities = [var.REQ_COMPATIBILITY]
+#   cpu                      = var.DB_CPU
+#   memory                   = var.DB_MEMORY
+#   execution_role_arn       = var.EXECUTION_ROLE_ARN
 
-  container_definitions = jsonencode([
-    {
-      name      = "${var.ENVIRONMENT}-${var.SERVICE_NAME}-${var.CONTAINER}"
-      image     = var.IMAGE_URL
-      essential = var.ESSENTIAL_VALUE
-      portMappings = [
-        {
-          containerPort = var.DB_PORT
-          hostPort      = var.DB_PORT
-          protocol      = var.TCP_PROTOCOL
-        }
-      ]
-      environment = [
-        { name = "POSTGRES_DB", value = var.POSTGRES_DB },
-        { name = "POSTGRES_USER", value = var.POSTGRES_USER },
-        { name = "POSTGRES_PASSWORD", value = var.POSTGRES_PASSWORD }
-      ]
-      logConfiguration = {
-        logDriver = var.LOG_DRIVER
-        options = {
-          "awslogs-group"         = aws_cloudwatch_log_group.database_log_group.name
-          "awslogs-region"        = var.REGION
-          "awslogs-stream-prefix" = var.ECS_PREFIX
-        }
-      }
-    }
-  ])
-}
+#   container_definitions = jsonencode([
+#     {
+#       name      = "${var.ENVIRONMENT}-${var.SERVICE_NAME}-${var.CONTAINER}"
+#       image     = var.IMAGE_URL
+#       essential = var.ESSENTIAL_VALUE
+#       portMappings = [
+#         {
+#           containerPort = var.DB_PORT
+#           hostPort      = var.DB_PORT
+#           protocol      = var.TCP_PROTOCOL
+#         }
+#       ]
+#       environment = [
+#         { name = "POSTGRES_DB", value = var.POSTGRES_DB },
+#         { name = "POSTGRES_USER", value = var.POSTGRES_USER },
+#         { name = "POSTGRES_PASSWORD", value = var.POSTGRES_PASSWORD }
+#       ]
+#       logConfiguration = {
+#         logDriver = var.LOG_DRIVER
+#         options = {
+#           "awslogs-group"         = aws_cloudwatch_log_group.database_log_group.name
+#           "awslogs-region"        = var.REGION
+#           "awslogs-stream-prefix" = var.ECS_PREFIX
+#         }
+#       }
+#     }
+#   ])
+# }
 
 resource "aws_ecs_service" "database_service" {
-  name            = "${var.ENVIRONMENT}-${var.SERVICE_NAME}-${var.SERVICE}"
-  cluster         = var.CLUSTER_ID
-  task_definition = aws_ecs_task_definition.database_task.arn
-  launch_type     = var.REQ_COMPATIBILITY
-  desired_count   = var.DESIRED_COUNT
+  name    = "${var.ENVIRONMENT}-${var.SERVICE_NAME}-${var.SERVICE}"
+  cluster = var.CLUSTER_ID
+  # task_definition = aws_ecs_task_definition.database_task.arn
+  launch_type   = var.REQ_COMPATIBILITY
+  desired_count = var.DESIRED_COUNT
 
   network_configuration {
     subnets          = var.PRIVATE_SUBNETS
